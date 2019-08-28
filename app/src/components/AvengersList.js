44171c3ab8ec939063f103e3d289807a91778a68
stevekutz <stkutz@gmail.com>
2019-08-27 23:35:13 -0500

    exp with layout on AvengersList

--- app/src/components/AvengersList.js
+++ app/src/components/AvengersList.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Container, Card, Image, Grid} from 'semantic-ui-react';
+import {Container, Card, Image, Grid, Label} from 'semantic-ui-react';
 
 import avengers from '../data/avengerInfo';
 
+// mini tiny small medium large big huge massive
 const AvengersList = (props) => {
     return (
         <Container>
         <h2>{props.listProp}</h2>
-            <Grid columns = '3' padded >
-            {avengers.map(hero => (
-                <Card style = {{cursor: 'pointer'}} >
-                    <Image src = {hero.thumbnail} size  = 'tiny'/>
-                    <Link to = {`/avengers/${hero.id}`}>{hero.name} </Link>
-                        
-                </Card>
-            ))}
+        <Grid style = {{border: `1px solid green`}}   columns = '4'>
 
-            </Grid>
+        {avengers.map(hero => (
+                <Card key = {hero.id} >
+                    <Card.Content style = {{cursor: 'pointer'}}>
+                
+                        <Grid stretched>
+                            <Image src = {hero.thumbnail}  height = '150px'/>
+                            <Label>
+                            <Link to = {`/avengers/${hero.id}`}>{hero.name} </Link>
+                            </Label>
+                        
+                        </Grid>
+                    
+                    </Card.Content>
+                    
+                </Card>
+                
+                ))}
+                
+                </Grid>
         </Container>
+
     )
 }
 
